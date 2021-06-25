c9fa11fcb5791c954522e6864e00311f9c0f63b3
ghosind <ghosind@gmail.com>
2021-06-25 22:13:51 +0800

    Bump version to v0.2.0.

--- lib/pool.d.ts
+++ lib/pool.d.ts
@@ -6,13 +6,19 @@ interface TaskPoolOptions {
      * A positive number or zero to indicates maximum concurrency limit number, no limitation if it's
      * set to 0 (similar with `Promise.all()`).
      */
-    concurrency: number;
+    concurrency?: number;
+    /**
+     * Is throws error and terminates tasks if some task threw error, default true.
+     */
+    throwsError?: boolean;
 }
 export declare class TaskPool extends EventEmitter {
     private readonly DEFAULT_CONCURRENCY;
     private tasks;
     private concurrency;
+    private throwsError;
     private resolutions;
+    private errors;
     private isRunning;
     private queue;
     private running;
@@ -30,7 +36,12 @@ export declare class TaskPool extends EventEmitter {
      * Add a task or tasks array into pool.
      * @param task Task instance or task instances array.
      */
-    addTask(task: Task | Task[]): void;
+    addTask(task: Task): number;
+    /**
+     * Add tasks array into pool.
+     * @param tasks Task instances array.
+     */
+    addTasks(tasks: Task | Task[]): number[];
     /**
      * Execute all tasks in the pool.
      * @async
@@ -41,9 +52,18 @@ export declare class TaskPool extends EventEmitter {
      * @param concurrency The maximum concurrency tasks number.
      */
     setConcurrency(concurrency: number): void;
+    /**
+     * Gets execution errors of last time.
+     */
+    getErrors(): (Error | undefined)[];
+    /**
+     * Get task instance by it's id.
+     */
+    getTask(id: number): Task | null;
     private runTask;
     private nextTask;
     private run;
     private checkRunningState;
+    private handleError;
 }
 export {};
